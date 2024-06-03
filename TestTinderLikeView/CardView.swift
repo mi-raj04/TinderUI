9a85428b90c9b67bb7ee1dc0925b625aedd37fd0
raj saija <raj.saija@mindinventory.com>
2024-06-03 11:25:02 +0530

    first commit

---
+++ TestTinderLikeView/CardView.swift
@@ -0,0 +1,40 @@
+//
+//  CardView.swift
+//  TestTinderLikeView
+//
+//  Created by mind on 03/06/24.
+//
+
+import SwiftUI
+
+struct CardView: View, Identifiable {
+    let id = UUID()
+    let image: String
+    let title: String
+    
+    var body: some View {
+        Image(image)
+            .resizable()
+            .scaledToFill()
+            .frame(minWidth: 0, maxWidth: .infinity)
+            .cornerRadius(10)
+            .padding(.horizontal, 15)
+            .overlay(alignment: .bottom) {
+                VStack {
+                    
+                    Text(title)
+                        .font(.system(.headline, design: .rounded))
+                        .fontWeight(.bold)
+                        .padding(.horizontal, 30)
+                        .padding(.vertical, 10)
+                        .background(.white)
+                        .cornerRadius(5)
+                }
+                .padding([.bottom], 20)
+            }
+    }
+}
+
+#Preview {
+    CardView(image: "yosemite-usa", title: "Yosemite, USA")
+}
---
+++ TestTinderLikeView/TestTinderLikeViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestTinderLikeViewApp.swift
+//  TestTinderLikeView
+//
+//  Created by mind on 03/06/24.
+//
+
+import SwiftUI
+
+@main
+struct TestTinderLikeViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
